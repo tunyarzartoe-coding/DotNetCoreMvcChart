92817f0844531a5ae090bcb8c8a9a80fb3cb0522
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-06 21:03:29 +0630

    [Complete] ApexChart - Basic Line Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/LineChartController.cs
@@ -0,0 +1,16 @@
+﻿using DotNetCoreMvcChart.ApexChartsApp.Models.LieChart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
+{
+    public class LineChartController : Controller
+    {
+        public IActionResult BasicLineChart()
+        {
+            BasicLineChartModel model = new BasicLineChartModel();
+            model.Datas = new List<int>() { 10, 41, 35, 51, 49, 62, 69, 91, 148 };
+            model.Categories = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep" };
+            return View(model);
+        }
+    }
+}
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/LieChart/BasicLineChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.LieChart
+{
+    public class BasicLineChartModel
+    {
+        public List<int> Datas { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
