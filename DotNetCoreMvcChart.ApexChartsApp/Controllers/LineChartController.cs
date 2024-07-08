80265c5658a56b5e11027932ee6cfe0e522792a2
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-08 22:06:21 +0630

    [Complete] ApexChart - Logarithmic Line Chart

--- DotNetCoreMvcChart.ApexChartsApp/Controllers/LineChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/LineChartController.cs
@@ -44,19 +44,19 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
             return View(model);
         }
 
-        public  IActionResult GradientLineChart()
+        public IActionResult GradientLineChart()
         {
             GradientLineChartModel model = new GradientLineChartModel();
             model.Categories = new List<string>() { "1/11/2000", "2/11/2000", "3/11/2000", "4/11/2000", "5/11/2000", "6/11/2000", "7/11/2000", "8/11/2000", "9/11/2000", "10/11/2000", "11/11/2000", "12/11/2000", "1/11/2001", "2/11/2001", "3/11/2001", "4/11/2001", "5/11/2001", "6/11/2001" };
             model.Data = new List<int>() { 4, 3, 10, 9, 29, 19, 22, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5 };
-            return View(model);  
+            return View(model);
         }
         public IActionResult DataLabelLineChart()
         {
             DataLabelLineChartModel model = new DataLabelLineChartModel();
             model.Categories = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul" };
-            model.Series = new List<Datas> ()
-            { 
+            model.Series = new List<Datas>()
+            {
                 new Datas()
                 {
                     Name = "High - 2013",
@@ -89,7 +89,7 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
         public IActionResult MissingDataLineChart()
         {
             MissingDataLineChartModel model = new MissingDataLineChartModel();
-            model.Series = new List<MissingData> 
+            model.Series = new List<MissingData>
             {
                 new MissingData()
                 {
@@ -108,6 +108,129 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
                 },
             };
             model.Labels = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+            return View(model);
+        }
+        public IActionResult LogarithmicLineChart()
+        {
+            LogarithmicLineChartModel model = new LogarithmicLineChartModel();
+            model.Datas = new List<LineData>()
+            {
+                new LineData()
+                {
+                    X = 1994,
+                    Y = 2543763
+                },
+                new LineData()
+                {
+                    X = 1995,
+                    Y = 4486659
+                },
+                new LineData()
+                {
+                    X = 1996,
+                    Y = 7758386
+                },
+                new LineData()
+                {
+                    X = 1997,
+                    Y = 12099221
+                },
+                new LineData()
+                {
+                    X = 1998,
+                    Y = 18850762
+                },
+                new LineData()
+                {
+                    X = 1999,
+                    Y = 28153765
+                },
+                new LineData()
+                {
+                    X = 2000,
+                    Y = 41479495
+                },
+                new LineData()
+                {
+                    X = 2001,
+                    Y = 50229224
+                },
+                new LineData()
+                {
+                    X = 2002,
+                    Y = 2543763
+                },
+                new LineData()
+                {
+                    X = 2003,
+                    Y = 78143598
+                },
+                new LineData()
+                {
+                    X = 2004,
+                    Y = 91332777
+                },
+                new LineData()
+                {
+                    X = 2005,
+                    Y = 103010128
+                },
+                new LineData()
+                {
+                    X = 2006,
+                    Y = 116291681
+                },
+                new LineData()
+                {
+                    X = 2007,
+                    Y = 137322698
+                },
+                new LineData()
+                {
+                    X = 2008,
+                    Y = 157506752
+                },
+                new LineData()
+                {
+                    X = 2009,
+                    Y = 176640381
+                },
+                new LineData()
+                {
+                    X = 2010,
+                    Y = 202320297
+                },
+                new LineData()
+                {
+                    X = 2011,
+                    Y = 223195735
+                },
+                new LineData()
+                {
+                    X = 2012,
+                    Y = 249473624
+                },
+                new LineData()
+                {
+                    X = 2013,
+                    Y = 272842810
+                },
+                new LineData()
+                {
+                    X = 2014,
+                    Y = 295638556
+                },
+                new LineData()
+                {
+                    X = 2015,
+                    Y = 318599615
+                },
+                new LineData()
+                {
+                    X = 2016,
+                    Y = 342497123
+                }
+            };
             return View(model);
         }
     }
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/LineChart/LogarithmicLineChartModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.LineChart
+{
+    public class LogarithmicLineChartModel
+    {
+        public List<LineData> Datas { get; set; }
+    }
+    public class LineData
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
