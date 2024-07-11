cd6ff5d3e4c63622fc3be41b77643e60c48de9ff
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-11 13:23:52 +0630

    [Complete] Bar Chart - Grouped Bar Chart

--- DotNetCoreMvcChart.ApexChartsApp/Controllers/BarChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/BarChartController.cs
@@ -16,5 +16,13 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
             model.Datas = new List<int>() { 400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380 };
             return View(model);
         }
+        public IActionResult GroupedBarChart()
+        {
+            GroupedBarChartModel model = new GroupedBarChartModel();
+            model.Categories = new List<int>() { 2001, 2002, 2003, 2004, 2005, 2006, 2007};
+            model.Data1 = new List<int>() { 44, 55, 41, 64, 22, 43, 21 };
+            model.Data2 = new List<int>() { 53, 32, 33, 52, 13, 44, 32 };
+            return View(model);
+        }
     }
 }
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/BarChart/GroupedBarChartModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.BarChart
+{
+    public class GroupedBarChartModel
+    {
+        public List<int> Categories { get; set; }
+        public List<int> Data1 { get; set; }
+        public List<int> Data2 { get; set; }
+
+    }
+}
