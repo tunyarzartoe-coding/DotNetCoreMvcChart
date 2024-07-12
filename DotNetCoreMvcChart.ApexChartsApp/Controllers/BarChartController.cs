96181a94d9293729b4b40634cbbf71426de45181
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-12 17:07:50 +0630

    [Complete] Bar Chart - Maker Bar Chart

--- DotNetCoreMvcChart.ApexChartsApp/Controllers/BarChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/BarChartController.cs
@@ -1,6 +1,7 @@
 ﻿using DotNetCoreMvcChart.ApexChartsApp.Models;
 using DotNetCoreMvcChart.ApexChartsApp.Models.BarChart;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System.Xml.Linq;
 
 namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
@@ -181,20 +182,143 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
                 new NegativeBarChartData()
                 {
                    Name ="Males",
-                   Data = new List<double> (){0.4, 0.65, 0.76, 0.88, 1.5, 2.1, 2.9, 3.8, 3.9, 4.2, 4, 4.3, 4.1, 4.2, 4.5,
-                   3.9, 3.5, 3}
+                   Data = new List<double> ()
+                   {
+                       0.4, 0.65, 0.76, 0.88, 1.5, 2.1, 2.9, 3.8, 3.9, 4.2, 4, 4.3, 4.1, 4.2, 4.5,
+                       3.9, 3.5, 3
+                   }
                 },
                 new NegativeBarChartData()
                 {
                    Name ="Females",
-                   Data = new List<double> (){-0.8, -1.05, -1.06, -1.18, -1.4, -2.2, -2.85, -3.7, -3.96, -4.22, -4.3, -4.4,
-            -4.1, -4, -4.1, -3.4, -3.1, -2.8}
+                   Data = new List<double> ()
+                   {
+                       -0.8, -1.05, -1.06, -1.18, -1.4, -2.2, -2.85, -3.7, -3.96, -4.22, -4.3, -4.4,
+                       -4.1, -4, -4.1, -3.4, -3.1, -2.8
+                   }
                 },
             };
             model.Categories = new List<string>()
             {
                     "85+", "80-84", "75-79", "70-74", "65-69", "60-64", "55-59", "50-54", "45-49",
                     "40-44", "35-39", "30-34", "25-29", "20-24", "15-19", "10-14","5-9","0-4"
+            };
+            return View(model);
+        }
+        public IActionResult MakerBarChart()
+        {
+            MakerBarChartModel model = new MakerBarChartModel();
+            model.Name = "Actual";
+            model.Data = new List<MakerData>
+            {
+                new MakerData()
+                {
+                    X = "2011",
+                    Y=12,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 14,
+                            StrokeWidth= 2,
+                            StrokeDashArray= 2,
+                            //StrokeLineCap="",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
+                new MakerData()
+                {
+                    X = "2012",
+                    Y=44,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 54,
+                            StrokeWidth= 5,
+                            StrokeDashArray= 10,
+                            //StrokeLineCap="",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
+                new MakerData()
+                {
+                    X = "2013",
+                    Y=54,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 52,
+                            StrokeWidth= 10,
+                            StrokeDashArray= 0,
+                            StrokeLineCap="round",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
+                new MakerData()
+                {
+                    X = "2014",
+                    Y=66,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 61,
+                            StrokeWidth= 10,
+                            StrokeDashArray= 0,
+                            StrokeLineCap="round",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
+                new MakerData()
+                {
+                    X = "2015",
+                    Y=81,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 66,
+                            StrokeWidth= 10,
+                            StrokeDashArray= 0,
+                            StrokeLineCap="round",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
+                new MakerData()
+                {
+                    X = "2016",
+                    Y=67,
+                    Goals = new List<Goals>
+                    {
+                        new Goals()
+                        {
+                            Name= "Expected",
+                            Value= 70,
+                            StrokeWidth= 5,
+                            StrokeDashArray= 10,
+                            //StrokeLineCap="",
+                            StrokeColor= "#775DD0"
+                        }
+                    }
+
+                },
             };
             return View(model);
         }
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/BarChart/MakerBarChartModel.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.BarChart
+{
+    public class MakerBarChartModel
+    {
+        public string Name { get; set; }
+        public List<MakerData> Data { get; set; }
+    }
+    public class MakerData
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+        public List<Goals> Goals { get; set; }
+
+    }
+    public class Goals
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int StrokeWidth { get; set; }
+        public int StrokeDashArray { get; set; }
+        public string StrokeLineCap { get; set; }
+        public string StrokeColor { get; set; }
+    }
+
+}
