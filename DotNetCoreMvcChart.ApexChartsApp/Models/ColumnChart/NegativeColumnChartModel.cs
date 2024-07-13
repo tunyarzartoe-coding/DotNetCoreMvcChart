0cb096c6bb6c7751730f7b7144a3cfd2def7ef75
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-13 12:05:59 +0630

    [Complete] Column Chart - Negative Column Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/ColumnChart/NegativeColumnChartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.ColumnChart
+{
+    public class NegativeColumnChartModel
+    {
+        public string Name { get; set; }
+        public List<double> Data { get; set; }
+        public List<string> Categories { get; set; }
+    }
+}
