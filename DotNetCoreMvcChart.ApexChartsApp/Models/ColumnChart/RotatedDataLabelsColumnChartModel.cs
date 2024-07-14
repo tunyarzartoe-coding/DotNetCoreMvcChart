2e603ddcce5b3aa5202dc244018caffc04ee686f
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-14 21:54:28 +0630

    [Complete] Column Chart - Rotated Data Labels Column Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/ColumnChart/RotatedDataLabelsColumnChartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.ColumnChart
+{
+    public class RotatedDataLabelsColumnChartModel
+    {
+        public string Name { get; set; }
+        public List<double> Datas { get; set; }
+        public List<string> Categories { get; set;}
+    }
+}
