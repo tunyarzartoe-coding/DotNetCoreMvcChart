1e900e178662d5a2af2a717446e25e7c39d671c0
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-15 21:22:54 +0630

    [Complete] Column Chart - Stacked Column with Line Chart

--- DotNetCoreMvcChart.ApexChartsApp/Controllers/ColumnChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/ColumnChartController.cs
@@ -708,13 +708,13 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
         public IActionResult StackedColumnChart()
         {
             StackedColumnChartModel model = new StackedColumnChartModel();
-            model.Categories = new List<string>() 
+            model.Categories = new List<string>()
             {
                     "01/01/2011 GMT", "01/02/2011 GMT", "01/03/2011 GMT", "01/04/2011 GMT",
                     "01/05/2011 GMT", "01/06/2011 GMT"
             };
-            model.SeriesData = new List<SeriesData>() 
-            { 
+            model.SeriesData = new List<SeriesData>()
+            {
                 new SeriesData()
                 {
                     Name = "PRODUCT A",
@@ -738,9 +738,9 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
             };
             return View(model);
         }
-        public IActionResult FullStackedColumnChart() 
+        public IActionResult FullStackedColumnChart()
         {
-            FullStackedColumnChartModel model = new  FullStackedColumnChartModel();
+            FullStackedColumnChartModel model = new FullStackedColumnChartModel();
             model.Categories = new List<string>()
             {
                     "2011 Q1", "2011 Q2", "2011 Q3", "2011 Q4", "2012 Q1", "2012 Q2",
@@ -763,6 +763,180 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
                     Name = "PRODUCT C",
                     Data = new List<int>(){ 11, 17, 15, 15, 21, 14, 15, 13 }
                 }
+            };
+            return View(model);
+        }
+        public IActionResult StackedColumnWithLineChart()
+        {
+            StackedColumnWithLineChartModel model = new StackedColumnWithLineChartModel();
+            model.Colors = new List<string>()
+            {
+                "#6a6ba5",
+                "#f9a867",
+                "#2a6b9b",
+                "#6c98b7",
+                "#abc4d6",
+                "#d5e2eb"
+            };
+            model.SeriesDatas = new List<SeriesDatas>()
+            {
+                new SeriesDatas()
+                {
+                    Name = "Line",
+                    Type = "line",
+                    Data = new List<double>()
+                    { 
+                        37,
+                        38.3,
+                        33.01,
+                        30,
+                        29.7,
+                        30,
+                        35.2,
+                        44,
+                        45,
+                        41,
+                        22,
+                        22.5,
+                        29.1,
+                        25.5,
+                        41.2,
+                        43.8,
+                        39,
+                        23,
+                        26,
+                        22,
+                        34,
+                        37,
+                        43,
+                        41,
+                        17
+                    }
+                },
+                new SeriesDatas()
+                {
+                    Name = "Bar1",
+                    Type = "bar",
+                    Data = new List<double>()
+                    {
+                        -1.57,
+                        -1.83,
+                        -0.29,
+                        2,
+                        0.23,
+                        3,
+                        0,
+                        -0.77,
+                        0,
+                        0,
+                        0,
+                        -0.16,
+                        -0.5,
+                        -0.75,
+                        -0.93,
+                        -0.93,
+                        -0.45,
+                        0,
+                        0,
+                        -0.23,
+                        -0.95,
+                        -1.12,
+                        -0.51,
+                        0.76
+                    }
+                },
+                new SeriesDatas()
+                {
+                    Name = "Bar2",
+                    Type = "bar",
+                    Data = new List<double>()
+                    {
+                        -2.99,
+                        -0.43,
+                        0.39,
+                        2.8,
+                        0.23,
+                        1.2,
+                        -0.06,
+                        -2.96,
+                        -2.74,
+                        -2.58,
+                        -1.79,
+                        -2.16,
+                        -1.9,
+                        -1.75,
+                        -2,
+                        -2.16,
+                        -1.75,
+                        -1.9,
+                        -2.74,
+                        -2.58,
+                        -3.8,
+                        -1.24,
+                        -0.01,
+                        1.89
+                    }
+                },
+                new SeriesDatas()
+                {
+                    Name = "Bar3",
+                    Type = "bar",
+                    Data = new List<double>()
+                    {
+                        0.09,
+                        4,
+                        4.83,
+                        6.86,
+                        3.86,
+                        5,
+                        1.7,
+                        -1.21,
+                        0,
+                        0.02,
+                        0.07,
+                        -0.15,
+                        -0.09,
+                        0,
+                        -0.61,
+                        -1.75,
+                        0,
+                        -0.43,
+                        -1.5,
+                        -0.66,
+                        -3.06,
+                        0.06,
+                        0.15,
+                        1.48
+                    }
+                },
+                new SeriesDatas()
+                {
+                    Name = "bar4",
+                    Type = "bar",
+                    Data = new List<double>()
+                    {
+                        5.95,
+                        5.86,
+                        0.92,
+                        -1.2,
+                        -2.73,
+                        -0.32,
+                        0.12,
+                        -0.55,
+                        0.78,
+                        0,
+                        -1.73,
+                        -2.52,
+                        -1.26,
+                        -0.8,
+                        -0.75,
+                        0,
+                        -3,
+                        0,
+                        0.29,
+                        0.3
+                    }
+                },
             };
             return View(model);
         }
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/ColumnChart/StackedColumnWithLineChartModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.ColumnChart
+{
+    public class StackedColumnWithLineChartModel
+    {
+        public List<string> Colors { get; set; }
+        public List<SeriesDatas> SeriesDatas { get; set; }
+    }
+    public class SeriesDatas
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public List<double> Data { get; set; }
+    }
+}
