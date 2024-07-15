5636f93daae5bbbe88e1a6cb50d4e2eb06a559de
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-15 20:53:39 +0630

    [Complete] Column Chart - Stacked Column Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/ColumnChart/StackedColumnChartModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.ColumnChart
+{
+    public class StackedColumnChartModel
+    {
+        public List<SeriesData> SeriesData { get; set; }
+        public List<string> Categories { get; set; }    
+    }
+    public class SeriesData
+    {
+        public string Name { get; set; }
+        public List<int> Data { get; set; }
+    }
+}
