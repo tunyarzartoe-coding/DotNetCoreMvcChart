fc42bb3a79c1f2e8bee1cc27a7cc2d40b64e0cef
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-17 11:49:45 +0630

    [Complete] Radar Chart - Basic Radar Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/RadarChartController.cs
@@ -0,0 +1,23 @@
+﻿using DotNetCoreMvcChart.ApexChartsApp.Models.RadarChart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
+{
+    public class RadarChartController : Controller
+    {
+        public IActionResult BasicRadarChart()
+        {
+            BasicRadarChartModel model = new BasicRadarChartModel();
+            model.Categories = new List<string>() { "January", "February", "March", "April", "May", "June" };
+            model.SeriesData = new List<SeriesData>() 
+            { 
+                new SeriesData()
+                {
+                    Name = "Series 1",
+                    Data = new List<int> { 80, 50, 30, 40, 100, 20 }
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/RadarChart/BasicRadarChartModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.RadarChart
+{
+    public class BasicRadarChartModel
+    {
+        public List<SeriesData> SeriesData { get; set; }
+        public List<string> Categories { get; set; }      
+    }
+    public class SeriesData
+    {
+        public string Name { get; set; }
+        public List<int> Data { get; set; }
+    }
+}
