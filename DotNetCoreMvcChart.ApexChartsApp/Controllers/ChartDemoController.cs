0825439529f0dc7455f4bcb7e3b2f83e881b0810
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-17 11:23:04 +0630

    [Complete] Chart Demo - Line Chart

---
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/ChartDemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
+{
+    public class ChartDemoController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
--- DotNetCoreMvcChart.ApexChartsApp/Controllers/LineChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/LineChartController.cs
@@ -10,6 +10,14 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
             BasicLineChartModel model = new BasicLineChartModel();
             model.Datas = new List<int>() { 10, 41, 35, 51, 49, 62, 69, 91, 148 };
             model.Categories = new List<string>() { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep" };
+            ViewBag.CodeSnippet = @"
+public IActionResult BasicLineChart()
+{
+    BasicLineChartModel model = new BasicLineChartModel();
+    model.Datas = new List<int>() { 10, 41, 35, 51, 49, 62, 69, 91, 148 };
+    model.Categories = new List<string>() { ""Jan"", ""Feb"", ""Mar"", ""Apr"", ""May"", ""Jun"", ""Jul"", ""Aug"", ""Sep"" };
+    return View(model);
+}";
             return View(model);
         }
 
