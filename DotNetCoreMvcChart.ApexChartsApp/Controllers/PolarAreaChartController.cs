0bee154dd9d8008507dc697cf15e301a28d9dc11
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-18 11:56:18 +0630

    [Complete] Polar Area Chart - Monochrome  Polar Area Chart

--- DotNetCoreMvcChart.ApexChartsApp/Controllers/PolarAreaChartController.cs
+++ DotNetCoreMvcChart.ApexChartsApp/Controllers/PolarAreaChartController.cs
@@ -11,5 +11,12 @@ namespace DotNetCoreMvcChart.ApexChartsApp.Controllers
             model.Series = new List<int>() { 14, 23, 21, 17, 15, 10, 12, 17, 21 };
             return View(model);
         }
+        public IActionResult MonochromePolarAreaChart()
+        {
+            MonochromePolarAreaChartModel model= new MonochromePolarAreaChartModel();
+            model.SerieDatas = new List<int>() { 42, 47, 52, 58, 65 };
+            model.Labels = new List<string>() { "Rose A", "Rose B", "Rose C", "Rose D", "Rose E" };
+            return View(model);
+        }
     }
 }
---
+++ DotNetCoreMvcChart.ApexChartsApp/Models/PolarAreaChart/MonochromePolarAreaChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetCoreMvcChart.ApexChartsApp.Models.PolarAreaChart
+{
+    public class MonochromePolarAreaChartModel
+    {
+        public List<string> Labels {  get; set; }
+        public List<int> SerieDatas {  get; set; }
+    }
+}
